3c9ab011ed962fba892524c78d2c9b20a280183f
Sanket Khapake <157945367+san7499@users.noreply.github.com>
2025-05-04 16:01:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def serve_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
